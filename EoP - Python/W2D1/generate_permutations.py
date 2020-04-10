1e568ecf05c28531487221926353f1b98d8f844b
“Eugene <eugenechung1209@gmail.com>
2020-04-10 18:27:17 -0400

    finish gasup and water trapped

--- EoP - Python/W2D1/generate_permutations.py
+++ EoP - Python/W2D1/generate_permutations.py
@@ -3,13 +3,42 @@
 
 # review 
 
-def generate_perms(arr, i = 0):
+# def generate_perms(arr, i = 0):
+#   result = []
+#   if i == len(arr) - 1:
+#     result.append(arr.copy())
+#     return
+#   for j in range(i, len(arr)):
+#     arr[i], arr[j] = arr[j], arr[i]
+#     generate_perms(arr, i + 1)
+#     arr[i], arr[j] = arr[j], arr[i]
+#   return result
+
+arr = [1,2]
+
+# recursively
+#          None  [2, 3, 5, 7]
+#           2     [3. 5. 7]
+#           3     [5, 7]
+#           5      [7]
+#           7      []
+
+
+def generate_permutations(arr):
   result = []
-  if i == len(arr) - 1:
-    result.append(arr.copy())
-    return
-  for j in range(i, len(arr)):
-    arr[i], arr[j] = arr[j], arr[i]
-    generate_perms(arr, i + 1)
-    arr[i], arr[j] = arr[j], arr[i]
+  def dfs(permutated, decision_arr):
+    if len(decision_arr) == 0: result.append(permutated[:]) # result = [[2,3,5,7]]
+    for num in decision_arr:
+      # decision = [3 ,5, 7] permutated = [2] => decision = [5, 7], permute = [2, 3] => decision = [7], permute [2,3,5]
+      permutated.append(num) 
+      # permutated = [2, 3] => [2, 3, 5] = [2, 3, 5, 7]
+      # print(permutated)
+
+      next_decision_arr = decision_arr[:] # next = [3, 5 ,7] = [5, 7] = [7]
+      next_decision_arr.remove(num) # next = [5, 7] = [7] = []
+      dfs(permutated, next_decision_arr) # => dfs([2], [3, 5, 7]) => dfs([2, 3], [5, 7]) => dfs([2,3,5], [7]) => dfs([2,3,5,7], [])
+      permutated.pop() # permutated = [2, 3, 5]
+  dfs([], arr)
   return result
+
+print(generate_permutations(arr))
