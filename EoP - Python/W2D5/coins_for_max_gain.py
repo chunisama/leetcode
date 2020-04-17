09797bc0b02b691a0fb33e5243a5b66eeb7cf947
“Eugene <eugenechung1209@gmail.com>
2020-04-16 22:53:42 -0400

    review red and yellow problems

---
+++ EoP - Python/W2D5/coins_for_max_gain.py
@@ -0,0 +1,6 @@
+# Design an efficient algorithm for computing the maximum total value for 
+# the starting player in the pick-up-coins game.
+
+coins = [25, 5, 10, 5, 10, 5, 10, 25, 1, 25, 1,25 ,1 ,25, 5, 10]
+coins = [5, 25, 10, 1]
+def coins_for_max_gain(coins):
---
+++ EoP - Python/W2D5/knapsack_problem.py
@@ -0,0 +1,48 @@
+# Write a program for the knapsack problem that selects a subset of items that has 
+# maximum value and satisfies the weight constraint. All items have integer weights and values. 
+# Return the value of the subset.
+
+class Clock:
+  def __init__(self, weight, value):
+    self.weight = weight
+    self.value = value
+
+
+# example input = [[$60, 5oz], [$50, 3oz]. [$70, 4oz], [$30, 2oz]] ==> ($80)  (3oz 20oz) given 5 oz constraint
+def knap_sack(clocks, weight_limit):
+    # outer for loop is the row    inner for loop is the col
+    tabBoard = [[0 for i in range(weight_limit + 1)]
+                for i in range(len(clocks) + 1)]
+    # x represents the clock
+    for x in range(1, len(clocks)+1): 
+        # y represents the weight constraint
+        for y in range(1, weight_limit + 1):
+            clock = clocks[x - 1]
+            # print(clock.weight)
+            # print(x)
+            if clock.weight > y:
+                tabBoard[x][y] = tabBoard[x - 1][y]
+            else:
+                tabBoard[x][y] = max(
+                    tabBoard[x - 1][y], (clock.value + tabBoard[x - 1][y - clock.weight]))
+    return tabBoard[-1][-1]
+
+
+#            0, 1, 2,  3 ,  4 ,  5
+#          [0, 0, 0,   0,   0,   0]
+# $60/5 oz [0, 0, 0,   0,   0, $60]
+# $50/3 oz [0, 0, 0, $50, $50, $60] 
+# $70/4 oz [0, 0, 0, $50, $70, $70]
+# $30/2 oz [0, 0, 30, $50, $70,$80]
+
+arr = [[60, 5], [50, 3], [70, 4], [30, 2]]
+arr2 = [[65, 20], [35, 8], [245, 60], [195, 55], [65, 40], [150, 70], [275, 85], [155, 25], [
+    120, 30], [320, 65], [75, 75], [40, 10], [200, 95], [100, 50], [220, 40], [99, 10]]
+clocks = []
+for ele in arr2:
+    clocks.append(Clock(ele[1], ele[0]))
+# print(clocks[0].weight)
+# print(clocks[0].value)
+
+print(knap_sack(clocks, 130))
+#Time complexity / space = o(m*n)
