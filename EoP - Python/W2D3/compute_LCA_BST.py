01aca6f8d20580da1143dca0fdc242e934cd131f
“Eugene <eugenechung1209@gmail.com>
2020-04-22 17:55:58 -0400

    do more bst problems

--- EoP - Python/W2D3/compute_LCA_BST.py
+++ EoP - Python/W2D3/compute_LCA_BST.py
@@ -3,13 +3,27 @@
 # Assume all keys are distinct. Nodes do not have references to their parents.
 
 # redo
+# O(h) => height of the BST
 
-def find_LCA_BST(tree, node_a, node_b):
-  while tree.data < node_a.data and tree.data > node_b.data:
-    # keep searching since tree is outside of [node_a, node_b]
-    while tree.data < node_a.data:
-      tree = tree.right # LCA must be the right child
-    while tree.data > node_b.data:
-      tree = tree.left # LCA must bet the left child
-  # now node_a.data <= tree.data and tree.data <= node_b.data
-  return tree
+# iterative
+def find_LCA_BST(root, node_a, node_b):
+  while root.data < node_a.data or root.data > node_b.data:
+    # keep searching since root is outside of [node_a, node_b]
+    while root.data < node_a.data:
+      root = root.right # LCA must be the right child
+    while root.data > node_b.data:
+      root = root.left # LCA must bet the left child
+  # now node_a.data <= root.data and root.data <= node_b.data
+  return root
+
+# recursive
+def find_LCA_BST(root, node_1, node_2):
+  if root > max(node_1.data, node_2.data):
+    # check left child
+    find_LCA_BST(root.left, node_1, node_2)
+  elif root < min(node_1.data, node_2.data):
+    # check right child
+    find_LCA_BST(root.right, node_1, node_2)
+    # found LCA since our current node will be less than node 2, and greater than node 1
+  else:
+    return root
