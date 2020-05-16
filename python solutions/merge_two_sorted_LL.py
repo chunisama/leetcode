fbba5517538bcb3af1c6d4fc19deab0611d04e39
“Eugene <eugenechung1209@gmail.com>
2020-05-15 20:07:43 -0400

    do more

---
+++ python solutions/merge_two_sorted_LL.py
@@ -0,0 +1,29 @@
+# Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+
+# Example:
+
+# Input: 1->2->4, 1->3->4
+# Output: 1->1->2->3->4->4
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        dummy = ListNode()
+        prev = dummy
+        
+        while l1 and l2:
+            if l1.val <= l2.val:
+                prev.next = l1
+                l1 = l1.next
+            else:
+                prev.next = l2
+                l2 = l2.next
+            prev = prev.next
+            
+        prev.next = l1 if l1 else l2
+        
+        return dummy.next
---
+++ python solutions/path_sum.py
@@ -0,0 +1,25 @@
+# Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
+
+# Note: A leaf is a node with no children.
+
+# Example:
+
+# Given the below binary tree and sum = 22,
+
+#       5
+#      / \
+#     4   8
+#    /   / \
+#   11  13  4
+#  /  \      \
+# 7    2      1
+# return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
+
+class Solution:
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+        if not root:
+            return False
+        sum -= root.val
+        if not root.left and not root.right:
+            return sum == 0
+        return self.hasPathSum(root.left, sum) or self.hasPathSum(root.right, sum)
