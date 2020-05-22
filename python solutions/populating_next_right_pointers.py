fa330b93470d70fbd39e198f42606c2817b5b494
“Eugene <eugenechung1209@gmail.com>
2020-05-21 22:08:24 -0400

    do more

--- python solutions/populating_next_right_pointers.py
+++ python solutions/populating_next_right_pointers.py
@@ -10,18 +10,18 @@
 
 # Initially, all next pointers are set to NULL.
 
- class Solution:
-    def connect(self, root: 'Node') -> 'Node':
-        if not root:
-            return
-        queue = [root]
-        while queue:
-            curr = queue.pop(0)
-            if curr.left and curr.right:
-                curr.left.next = curr.right
-                if curr.next:
-                    curr.right.next = curr.next.left
-                queue.append(curr.left)
-                queue.append(curr.right)
-        return root
+#  class Solution:
+#     def connect(self, root: 'Node') -> 'Node':
+#         if not root:
+#             return
+#         queue = [root]
+#         while queue:
+#             curr = queue.pop(0)
+#             if curr.left and curr.right:
+#                 curr.left.next = curr.right
+#                 if curr.next:
+#                     curr.right.next = curr.next.left
+#                 queue.append(curr.left)
+#                 queue.append(curr.right)
+#         return root
             
