ff286e6cbc1c98c4199d104dd1c85ae870a574c4
“Eugene <eugenechung1209@gmail.com>
2020-05-23 20:19:49 -0400

    do more

---
+++ python solutions/reverse_int.py
@@ -0,0 +1,27 @@
+# Given a 32-bit signed integer, reverse digits of an integer.
+
+# Example 1:
+
+# Input: 123
+# Output: 321
+# Example 2:
+
+# Input: -123
+# Output: -321
+# Example 3:
+
+# Input: 120
+# Output: 21
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        sign = -1 if x < 0 else 1
+        res = 0
+        x = abs(x)
+        while x: #1
+            res = res * 10 + (x % 10) # 20 + 1
+            x = x//10 # 1
+        if res > (2**31)-1 or res < -2**31:
+            return 0
+        return res * sign
+        
